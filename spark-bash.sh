f7977ddb2e4d409aeaba5842d227f19cce809945
BuildTools <damian@famaf.unc.edu.ar>
2023-04-19 20:34:32 -0300

    From main

--- spark-bash.sh
+++ spark-bash.sh
@@ -1,5 +1,45 @@
 #!/bin/bash
 
-TAG=damianbarsotti/spark:3.3.1
+TAG=damianbarsotti/spark-py:v3.3.1.0
 
-minikube kubectl -- run -it --rm --image=$TAG --restart=Never --image-pull-policy=Never spark-bash -- /bin/bash
+#minikube kubectl -- run -it --rm --image=$TAG --restart=Never spark-bash -- /bin/bash
+
+minikube kubectl -- run spark-bash -it --rm --image=$TAG --overrides='
+{
+	"kind": "Pod",
+	"apiVersion": "v1",
+	"metadata": {
+		"name": "spark-bash",
+		"namespace": "spark-demo"
+    },
+    "spec": {
+        "containers": [
+            {
+                "name": "spark-bash",
+                "image": "'$TAG'",
+                "command": ["/opt/entrypoint.sh"],
+                "args": ["/bin/bash"],
+                "stdin": true,
+                "tty": true,
+                "volumeMounts": [
+                    {
+                        "name": "shared-volume",
+                        "mountPath": "/files"
+                    }
+                ],
+                "terminationMessagePolicy": "FallbackToLogsOnError",
+                "imagePullPolicy": "Always"
+            }
+        ],
+        "restartPolicy": "Never",
+        "volumes": [
+            {
+                "name": "shared-volume",
+                "hostPath": {
+                    "path": "/files/"
+                }
+            }
+        ]
+    }
+}
+'
